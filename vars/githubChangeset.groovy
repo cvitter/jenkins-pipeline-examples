26add6a09366566246977d047a7732d4c471b6e3
Craig Vitter <craig.vitter@gmail.com>
2017-08-18 13:50:12 -0400

    Add githubChangeset library, add test for that library

---
+++ vars/githubChangeset.groovy
@@ -0,0 +1,26 @@
+// vars/githubChangeset.groovy
+// Iterates over commits since the last build and outputs them 
+// in the following format via echo:
+//    - Commit message [author]
+
+def getChangeset() {
+	def changeString = ""
+ 	def changeLogSets = currentBuild.changeSets
+ 	
+ 	// Iterate over changes and add them to changeString
+ 	for (int i = 0; i < changeLogSets.size(); i++) {
+		def entries = changeLogSets[i].items
+ 		for (int j = 0; j < entries.length; j++) {
+ 			def entry = entries[j]
+ 			changeString += " - ${entry.msg} [${entry.author}]\n"
+ 		}
+ 	}
+
+ 	if (!changeString) {
+ 		changeString = " - No new changes\n"
+ 	}
+ 	
+ 	changeString = "SCM Changes:\n------------------------------------------------------------\n" + 
+ 		changeString + "-------------------------------------------------------------";
+ 	echo changeString
+}
